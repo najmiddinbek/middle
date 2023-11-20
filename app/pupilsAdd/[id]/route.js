9de9aef4f3dbcd12c08fc8b75806cc806410ce65
najmiddin <turgunpolatov3gmail.com>
2023-11-20 08:11:03 +0500

    commit

--- app/pupilsAdd/[id]/route.js
+++ app/pupilsAdd/[id]/route.js
@@ -1,4 +1,4 @@
-import connectMongoDB from "../../../lib/mongodb";
+import { connectMongoDB } from "../../../lib/mongodb";
 import Topic from "../../../models/28-maktab";
 import { NextResponse } from "next/server";
 
